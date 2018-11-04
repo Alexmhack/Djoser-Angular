c921a404887b87de530f7b82ca39334c6189260a
alexHooper <alexmohack@gmail.com>
2018-11-04 23:21:28 +0530

    correct import errors

--- backend/settings.py
+++ backend/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import datetime
 
 from decouple import config
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
