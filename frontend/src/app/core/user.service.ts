69a2ed78ef026cab45307bbbd51e73259aac5707
alexHooper <alexmohack@gmail.com>
2018-11-04 23:13:46 +0530

    generate service for users

---
+++ frontend/src/app/core/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+}
